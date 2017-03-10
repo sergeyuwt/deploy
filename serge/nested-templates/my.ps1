c35b6dc7eacf815d2fb9692e2c21f011c672b0a4
Serge Yu <syu@teradici.com>
2017-03-10 11:46:31 -0800

    pass parameter

--- serge/nested-templates/my.ps1
+++ serge/nested-templates/my.ps1
@@ -4,6 +4,6 @@ $azureRGName = "syurg2" #keep it short and with no special characters and no cap
 
 New-AzureRmResourceGroup -Name $azureRGName -Location "West US"
 
-New-AzureRmResourceGroupDeployment -DeploymentName "syudeploy1" -ResourceGroupName $azureRGName -TemplateFile "main.json" -TemplateParameterFile "my.azuredeploy.parameters.json"
+New-AzureRmResourceGroupDeployment -DeploymentName "syudeploy1" -ResourceGroupName $azureRGName -TemplateUri "https://raw.githubusercontent.com/sergeyuwt/deploy/master/serge/nested-templates/main.json" -TemplateParameterUri "https://raw.githubusercontent.com/sergeyuwt/deploy/master/serge/nested-templates/my.azuredeploy.parameters.json"
 
 #Test-AzureRmResourceGroupDeployment -ResourceGroupName $azureRGName -TemplateFile "new-agent.json" -TemplateParameterFile "my.azuredeploy.parameters.json"
