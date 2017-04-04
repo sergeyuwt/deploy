886bb9b6d230b439c2d5456fded56ff3a906a945
Serge Yu <syu@teradici.com>
2017-04-04 14:36:26 -0700

    asdf

--- deployments/azure/ga/Install-NvidiaDriver.ps1
+++ deployments/azure/ga/Install-NvidiaDriver.ps1
@@ -2,18 +2,10 @@
 Configuration InstallNvidiaDriver
 {
 	param(
-     	[Parameter(Mandatory=$true)]
-     	[String] $sourceUrl,
-
      	[Parameter(Mandatory=$false)]
-        [String] $LocalDLPath = "$env:systemdrive\WindowsAzure\PCoIPAgentInstaller",
-
-     	[Parameter(Mandatory=$false)]
-        [String] $nvidiaInstaller = "nvidia-driver.zip"
+     	[String] $sourceUrl ="https://teradeploy.blob.core.windows.net/binaries/369.71_win10_64bit_international-1015172-02.zip"
     )
 	
-	#Import-DscResource -ModuleName xPSDesiredStateConfiguration
-
     Node "localhost"
     {
         LocalConfigurationManager
@@ -21,8 +13,18 @@ Configuration InstallNvidiaDriver
             RebootNodeIfNeeded = $true
         }
 
+        File Download_Directory 
+        {
+            Ensure          = "Present"
+            Type            = "Directory"
+            DestinationPath = "C:\WindowsAzure\NvidiaInstaller"
+        }
+
+
         Script InstallNvidiaDriver
         {
+            DependsOn  = "[File]Download_Directory"
+
             GetScript  = { @{ Result = "Install_Nvidia" } }
 
             TestScript = {
@@ -31,26 +33,21 @@ Configuration InstallNvidiaDriver
 
             SetScript  = {
                 Write-Verbose "Downloading Nvidia driver"
-                $LocalDLPath = $using:LocalDLPath
-                $nvidiaInstaller = $using:nvidiaInstaller
-                $destFile = "$LocalDLPath\$nvidiaInstaller"
-
+                $destFile = "c:\WindowsAzure\NvidiaInstaller\nvidia-driver.zip"
                 $sourceUrl = $using:sourceUrl
                 Invoke-WebRequest $sourceUrl -OutFile $destFile
 
-                #Unzip "D:\Downloadinstallers\NVAzureDriver.zip" "D:\NVIDIAazure"
-
                 Write-Verbose "Unzipping Nvidia driver"
-
-				$localNvidiaPath = "$env:systemdrive\nvidia"
-                Expand-Archive $destFile -DestinationPath $localNvidiaPath
+				$localNvidiaPath = "c:\nvidia"
+                Expand-Archive $destFile -DestinationPath $localNvidiaPath -Force
 
                 Write-Verbose "Starting to Install Nvidia driver"
+                Set-Location $localNvidiaPath
+                Set-ExecutionPolicy Unrestricted -force
+                .\setup.exe -s -noreboot -clean
+                Start-Sleep -s 180
 
-                $setupFile = $localNvidiaPath + "\setup.exe"
-                Start-Process -FilePath $destFile -setupFile "/S /nopostreboot" -PassThru -Wait
-				
-	            Write-Verbose "Finished Nvidia driver Installation"
+                Write-Verbose "Finished Nvidia driver Installation"
             }
         }
     }
