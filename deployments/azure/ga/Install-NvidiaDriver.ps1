02e2cacdcb0de428f78ff57d97a9e58407c9d337
Serge Yu <syu@teradici.com>
2017-04-04 10:19:31 -0700

    afasdf

--- deployments/azure/ga/Install-NvidiaDriver.ps1
+++ deployments/azure/ga/Install-NvidiaDriver.ps1
@@ -32,6 +32,8 @@ Configuration InstallNvidiaDriver
             SetScript  = {
                 Write-Verbose "Downloading Nvidia driver"
                 $destFile = "$LocalDLPath\$nvidiaInstaller"
+                #temp for test
+                $sourceurl = "https://teradeploy.blob.core.windows.net/binaries/369.71_win10_64bit_international-1015172-02.zip"
                 Invoke-WebRequest $sourceUrl -OutFile $destFile
 
                 #Unzip "D:\Downloadinstallers\NVAzureDriver.zip" "D:\NVIDIAazure"
